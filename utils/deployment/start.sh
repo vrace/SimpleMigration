1747ba227be4cbbabb554b89a3475a014a68b3aa
wane <vrace_studios@hotmail.com>
2022-10-20 23:53:21 +0800

    keep container after exit

--- utils/deployment/start.sh
+++ utils/deployment/start.sh
@@ -11,4 +11,4 @@ DB_USER="postgres"
 DB_PASSWORD="postgres"
 DB_DATABASE="postgres"
 
-docker run --rm -v $(pwd)/$DATA_IN:/data_in -v $(pwd)/$DATA_OUT:/data_out -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_DATABASE=$DB_DATABASE --name=$IMAGE_NAME --link=postgres $IMAGE_NAME
+docker run -v $(pwd)/$DATA_IN:/data_in -v $(pwd)/$DATA_OUT:/data_out -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_USER=$DB_USER -e DB_PASSWORD=$DB_PASSWORD -e DB_DATABASE=$DB_DATABASE --name=$IMAGE_NAME --link=postgres $IMAGE_NAME
